95c71c3da6c09d7ef1aabba55f8c029430c0053b
Jacky Chow <jckchow@users.noreply.github.com>
2017-05-05 10:50:32 -0600

    Update simple_exploration_controller.cpp

--- hector_exploration_controller/src/simple_exploration_controller.cpp
+++ hector_exploration_controller/src/simple_exploration_controller.cpp
@@ -28,7 +28,7 @@
 
 
 #include <ros/ros.h>
-#include <hector_path_follower/hector_path_follower.h>
+#include <hector_path_follower.h>
 #include <hector_nav_msgs/GetRobotTrajectory.h>
 
 class SimpleExplorationController
